7e23d4c29b8597386bb3199d2cb51edc935ef97b
Marko Radovic <markoradovic996@gmail.com>
2019-01-28 17:32:47 +0100

    Add unit tests for HelperMethodsService, started ZonePage unit testing

--- src/app/pages/transport-admin-page/zone-page/zone-page.component.spec.ts
+++ src/app/pages/transport-admin-page/zone-page/zone-page.component.spec.ts
@@ -1,25 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ZonePageComponent } from './zone-page.component';
+import {ToastrModule, ToastrService} from 'ngx-toastr';
+import {ZoneComponent} from './zone/zone.component';
+import {HttpClientModule} from '@angular/common/http';
+import {GenericService} from '../../../services/generic/generic.service';
+import {Observable} from 'rxjs';
+import {RouterModule} from '@angular/router';
 
 describe('ZonePageComponent', () => {
   let component: ZonePageComponent;
   let fixture: ComponentFixture<ZonePageComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ZonePageComponent ]
-    })
-    .compileComponents();
-  }));
+  let genericService: any;
 
   beforeEach(() => {
+    let genericServiceMock = {
+      getAll: jasmine.createSpy('getAll')
+        .and.returnValue(Observable.of([{}, {}, {}])),
+      delete: jasmine.createSpy('delete')
+        .and.returnValue(Observable.of())
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [ ZonePageComponent, ZoneComponent ],
+      imports: [RouterModule.forRoot([]), ToastrModule.forRoot() , HttpClientModule],
+      providers: [{ provide: 'BASE_API_URL', useValue: 'http://localhost:8080/api' },
+                  { provide: ToastrService },
+                  { provide: GenericService, useValue: genericServiceMock}]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ZonePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    genericService = TestBed.get(GenericService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should call getUsers and return list of users", async(() => {
+    component.ngOnInit();
+    expect(genericService.getAll).toHaveBeenCalled();
+    fixture.whenStable().then( () => {
+      expect(component.zones.length).toBe(3);
+    });
+  }));
 });
