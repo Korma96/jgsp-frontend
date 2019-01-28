76045b9156bfbc3e984535c7d06fa5e1f17e26a0
Jovo <sunjkajovo@gmail.com>
2019-01-28 22:58:51 +0100

    buy-ticket, passenger-page unit tests

--- src/app/buy-ticket/buy-ticket.component.spec.ts
+++ src/app/buy-ticket/buy-ticket.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { BuyTicketComponent } from './buy-ticket.component';
 import { FormsModule } from '@angular/forms';
@@ -10,23 +10,37 @@ import { ToastrService, ToastrModule } from 'ngx-toastr';
 import { PriceService } from '../services/price/price.service';
 import { TicketDto } from '../model/ticket-dto';
 import { Ticket } from '../model/ticket';
+import { PassengerPageComponent } from '../pages/passenger-page/passenger-page.component';
+import { of } from 'rxjs';
 
 describe('BuyTicketComponent', () => {
   let component: BuyTicketComponent;
   let fixture: ComponentFixture<BuyTicketComponent>;
   const relativeUrlForBuyTicket: string = 'relativeUrlForBuyTicket';
-  const ticketDto: TicketDto = { name: '1', hasZoneNotLine: false, ticketType: 'onetime', dayInMonthOrMonthInYear: -1 };
-  const ticket: Ticket = {startDateAndTime: 'not yet activated', endDateAndTime: 'not yet activated'}
+  const ticketDto1: TicketDto = { name: '1', hasZoneNotLine: false, ticketType: 'onetime', dayInMonthOrMonthInYear: -1 };
+  const ticketDto2: TicketDto = { name: '1', hasZoneNotLine: true, ticketType: 'daily', dayInMonthOrMonthInYear: -1 };
+  const zone: string = 'neka_zona';
+  const ticket: Ticket = {id: 1, ticketType: 'ONETIME', startDateAndTime: 'not yet activated', 
+    endDateAndTime: 'not yet activated', lineZone: '1', passengerType: 'STUDENT', price: 200.0};
+  const price: number = 2000.0;
+
   beforeEach(async(() => {
     const genericServiceMock = {
-      save: jasmine.createSpy('save').arguments(ticketDto)
-        .and.returnValue(Promise.resolve(new Student({
-            startDateAndTime: 1,
-            cardNumber: 'a123',
-            firstName: 'Petar',
-            lastName: 'Petrovic'            
-         	}))),
+      save: jasmine.createSpy('save')//.arguments(ticketDto1)
+        .and.returnValue(of(ticket))
     };
+
+    const successMessage = 'Success message!';
+    const errorMessage = 'Error message!';
+    const toastrServiceMock = {
+      success: jasmine.createSpy('success'), //.arguments = [successMessage],
+      error: jasmine.createSpy('error') //.arguments = [errorMessage]
+    };
+
+    /*const priceServiceMock = {
+      get: jasmine.createSpy('get')//.arguments = [ticketDto1.hasZoneNotLine, ticketDto1.ticketType, zone],
+        .and.returnValue(of(price)),
+    };*/
 
     TestBed.configureTestingModule({
       declarations: [ BuyTicketComponent, ModalDialogComponent ],
@@ -35,8 +49,7 @@ describe('BuyTicketComponent', () => {
         { provide: 'BASE_API_URL', useValue: 'http://localhost:8080/api' },
         { provide: GenericService, useValue: genericServiceMock }, 
         { provide: ToastrService, useValue: toastrServiceMock }, 
-        { provide: PriceService, useValue: priceServiceMock }, 
-        { provide: NgbCalendar, useValue: ngbCalendarMock }
+        PriceService // kasnije ce biti mokovan
       ]
     })
     .compileComponents();
@@ -46,9 +59,43 @@ describe('BuyTicketComponent', () => {
     fixture = TestBed.createComponent(BuyTicketComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    spyOn(component.boughtTicketEvent, 'emit');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('buyTicket test', fakeAsync(() => {
+    // component.ngOnInit();
+    //fixture.whenStable().then( () => {
+    component.ticket = ticketDto1;
+    component.buyTicket();
+
+    tick();
+
+    expect(component.boughtTicketEvent.emit).toHaveBeenCalledWith(ticket);
+    //});
+    //const currentNumOfTickets: number = passengerPageComponent.tickets.length;
+    //expect(oldNumOfTickets + 1).toBe(currentNumOfTickets);
+  }));
+
+  it('getPrice test', fakeAsync(() => {
+    component.ticket = ticketDto1;
+    component.selectedZone = {name: 'aaaa', transport: 'bus', lines: []};
+
+    const priceService = TestBed.get(PriceService);
+    spyOn(priceService, 'get').and.returnValue(of(price));
+
+    component.getPrice();
+
+    tick();
+
+    expect(priceService.get).toHaveBeenCalledWith(component.ticket.hasZoneNotLine, 
+      component.ticket.ticketType, component.selectedZone.name);
+    //});
+    //const currentNumOfTickets: number = passengerPageComponent.tickets.length;
+    //expect(oldNumOfTickets + 1).toBe(currentNumOfTickets);
+  }));
 });
--- src/app/pages/passenger-page/passenger-page.component.spec.ts
+++ src/app/pages/passenger-page/passenger-page.component.spec.ts
@@ -1,14 +1,81 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { PassengerPageComponent } from './passenger-page.component';
+import { GenericService } from 'src/app/services/generic/generic.service';
+import { HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
+import { Ticket } from 'src/app/model/ticket';
+import { ShowTicketsComponent } from 'src/app/show-tickets/show-tickets.component';
+import { PositionsOfVehiclesComponent } from 'src/app/positions-of-vehicles/positions-of-vehicles.component';
+import { BuyTicketComponent } from 'src/app/buy-ticket/buy-ticket.component';
+import { ShowScheduleComponent } from 'src/app/show-schedule/show-schedule.component';
+import { ChangeAccountTypeComponent } from 'src/app/change-account-type/change-account-type.component';
+import { ToastrService, ToastrModule } from 'ngx-toastr';
+import { ShowLinesComponent } from 'src/app/show-lines/show-lines.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalDialogComponent } from 'src/app/modal-dialog/modal-dialog.component';
+import { ShowTimesComponent } from 'src/app/show-times/show-times.component';
+import { CheckSliderComponent } from 'src/app/check-slider/check-slider.component';
+import { CheckDirective } from 'src/app/directives/check.directive';
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
+import { DirectionsMapComponent } from 'src/app/directions-map/directions-map.component';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 describe('PassengerPageComponent', () => {
   let component: PassengerPageComponent;
   let fixture: ComponentFixture<PassengerPageComponent>;
 
+  const ticket: Ticket = {id: 1, ticketType: 'ONETIME', startDateAndTime: 'not yet activated', 
+    endDateAndTime: 'not yet activated', lineZone: '1', passengerType: 'STUDENT', price: 200.0};
+  const tickets: Ticket[] = [
+    { id: 1, price: 200, passengerType: 'STUDENT', lineZone: 'aaa', 
+      endDateAndTime: '20.01.2018. 23:59', startDateAndTime: '20.01.2018. 06:00', ticketType: 'onetime'
+    },
+    { id: 2, price: 30, passengerType: 'PENSIONER', lineZone: 'bbb', 
+    endDateAndTime: '25.03.2018. 23:59', startDateAndTime: '25.03.2018. 12:45', ticketType: 'daily'
+    }
+  ];
+  const genericServiceMock = {
+    getAll: jasmine.createSpy('getAll').and.returnValue(of(tickets))
+  };
+
+  const successMessage = 'Success message!';
+  const errorMessage = 'Error message!';
+  const toastrServiceMock = {
+    success: jasmine.createSpy('success'), //.arguments = [successMessage],
+    error: jasmine.createSpy('error') //.arguments = [errorMessage]
+  };
+
+  const authenticationServiceMock = {
+    getToken: jasmine.createSpy('getToken').and.returnValue('jwt_token'),
+    login: jasmine.createSpy('login').and.returnValue(of(true)),
+    getCurrentUser: jasmine.createSpy('getCurrentUser').and.returnValue(
+      { username: 'aaa', password: 'aaa',
+        roles: ['PASSENGER', 'STUDENT'], token: 'token'}
+    )
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PassengerPageComponent ]
+      imports: [
+        NgbModule, HttpClientModule, ToastrModule.forRoot({ preventDuplicates: true }),
+         FormsModule, ReactiveFormsModule, AgmCoreModule, 
+         AgmCoreModule.forRoot({apiKey: 'AIzaSyCncyqJ42IAu6XewfdwvXyVmCOUyr30gWI' })
+      ],
+      declarations: [ PassengerPageComponent, ShowTicketsComponent, 
+        PositionsOfVehiclesComponent, BuyTicketComponent, ShowScheduleComponent, 
+        ChangeAccountTypeComponent, ShowLinesComponent, ModalDialogComponent, 
+        ShowTimesComponent, CheckSliderComponent, CheckDirective,
+        DirectionsMapComponent
+      ],
+      providers: [
+        { provide: 'BASE_API_URL', useValue: 'http://localhost:8080/api' },
+        { provide: GenericService, useValue: genericServiceMock }, 
+        { provide: ToastrService, useValue: toastrServiceMock },
+        { provide: AuthenticationService, useValue: authenticationServiceMock },
+        GoogleMapsAPIWrapper // kasnije ce biti mokovan
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +89,29 @@ describe('PassengerPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('getTIckets test', fakeAsync(() => {
+    const genericService = TestBed.get(GenericService);
+
+    component.getTickets();
+
+    tick();
+
+    expect(genericService.getAll).toHaveBeenCalled();
+    expect(component.tickets.length).toBe(2);
+  }));
+
+  it('boughtTicket test', fakeAsync(() => {
+    const genericService = TestBed.get(GenericService);
+    spyOn(component, 'sortTickets');
+    const oldNumOfTickets = component.tickets.length;
+    
+    component.boughtTicket(ticket);
+
+    const currentNumOfTickets = component.tickets.length;
+
+    expect(component.sortTickets).toHaveBeenCalled();
+    //expect(component.sortPartOfTickets).toHaveBeenCalledTimes(3);
+    expect(oldNumOfTickets + 1).toBe(currentNumOfTickets);
+  }));
 });
